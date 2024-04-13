99267658840414a327248f873afc06ebab16af7d
Sebastian-123-web <sebastian09_26@hotmail.com>
2024-04-13 15:33:44 -0500

    Se corrigio el problema que una estrella superaba el tamaño de la pantalla

--- js/index.js
+++ js/index.js
@@ -7,7 +7,10 @@ const stars = Object.values(star[0].children)
 console.log(screen.width)
 console.log(document.documentElement.scrollHeight)
 
+const t_monitor = document.documentElement.scrollHeight
+const l_monitor = document.documentElement.scrollWidth - 10
+
 stars.forEach((e,i) => {
-    stars[i].style.top = `${Math.ceil(Math.random() * (screen.width - 20))}px`
-    stars[i].style.left = `${Math.ceil(Math.random() * (document.documentElement.scrollWidth))}px`
+    stars[i].style.top = `${Math.ceil(Math.random() * t_monitor)}px`
+    stars[i].style.left = `${Math.ceil(Math.random() * l_monitor)}px`
 })
